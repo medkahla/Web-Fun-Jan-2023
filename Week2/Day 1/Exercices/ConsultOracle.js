30b312ca8ebc979a4db285539d47646cee780fdd
medkahla <elkahlamed@gmail.com>
2023-05-09 12:21:44 +0100

    appJS

---
+++ Week2/Day 1/Exercices/ConsultOracle.js
@@ -0,0 +1,29 @@
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+function picA(){
+    var rep = Math.ceil(Math.random()*lifesAnswers.length);
+        return lifesAnswers[rep];
+}
+
+var grep = picA(lifesAnswers);
+console.log("The good answer is " + grep);
