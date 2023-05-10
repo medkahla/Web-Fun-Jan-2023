0bfe34be6ec5b1557944ee1df733ea67ed2e991f
medkahla <elkahlamed@gmail.com>
2023-05-10 09:56:20 +0100

    Button clicker 80%

---
+++ Week2/Day 2/Core/ButtonClicker/scripts.js
@@ -0,0 +1,17 @@
+// for changing the login in logout
+var changelog = document.querySelector(".login");
+function changeT(){
+    changelog.innerText = "LogOut";
+}
+
+//to remove the button addDefinition
+var ad = document.querySelector(".add-def");
+function remAdd(){
+    ad.remove();
+}
+
+// alert on likes
+var likes = document.querySelector(".like")
+function alertLikes(){
+    alert("");
+}
