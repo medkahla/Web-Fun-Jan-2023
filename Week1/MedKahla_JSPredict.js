8ae2c44a2a01fea83ce0b81e84e56e5bcf977233
medkahla <elkahlamed@gmail.com>
2023-05-12 09:16:11 +0100

    JSPredictsI

---
+++ Week1/MedKahla_JSPredict.js
@@ -0,0 +1,28 @@
+// pred 1
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+}
+//  I was born in 1980
+
+// pred 2
+var birthYearInput = "1990";
+
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+}
+// I was born in 1990
+
+// pred 3
+var num1 = 10;
+var num2 = 20;
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+}
+// Summing Numbers!
+// num1 is: 10
+// num2 is: 20
+// 30
